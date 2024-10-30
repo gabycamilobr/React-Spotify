b1a3df3c240cdc61add3b3ae134e70d875769246
gabycamilobr <gabycamilobr@gmail.com>
2024-10-30 20:13:13 -0300

    agora sim

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,7 @@
 export default function Header() {
     return (
-        <header style={{ backgroundColor: '#a238fe' }} className=" w-full h-28 p-7 flex justify-around items-center"></header>
+        <header style={{ backgroundColor: '#a238fe' }} className=" w-full h-28 p-7 flex justify-around items-center">
+                <img src="/logg.svg" className="h-20 mt-2"/>
+        </header>
     )
 }
