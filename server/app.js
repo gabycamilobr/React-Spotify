547769b89e0977d09bcfa9a42baef6b059264b1f
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-30 11:44:58 -0300

    back-end

--- server/app.js
+++ server/app.js
@@ -4,7 +4,12 @@ import cors from 'cors';
 import artista from './models/Artistas.js';
 
 const app = express();
-app.use(cors(), express.json());
+app.use(cors(
+    {
+        origin: "*",
+        credentials: true
+    }
+), express.json());
 const conexao = await conectDb();
 
 conexao.on('error', (erro) => { 
@@ -24,6 +29,10 @@ app.get('/artistas/:id', async (req,res)=>{
     const artistas = await artista.findById(req.params.id);
     res.status(200).json(artistas);
 })
+
+app.get('/', (req, res) => {
+    res.send('Olá API!');
+});
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
